605fd2f8bbf5af73c1d3beee753d3b0a2151e04a
cesarh77 <cesarhenrique.tj@live.com>
2025-01-23 18:52:02 +0000

    Quase acabando

--- DesafioPaises.c
+++ DesafioPaises.c
@@ -3,15 +3,15 @@
 int main(){
 
 
-        int populacao;
-        float area;
+        int populacao, pontoturistico;
+        float area, pib;
         char estado, cidade[20];
-        char codigo[3];
+        char codigo[4];
         
 		
 		
         printf("Digite o estado: ");
-        scanf("%c", &estado);
+        scanf(" %c", &estado);
 
         printf("Digite o código da carta: ");
         scanf("%s", &codigo);
@@ -23,13 +23,21 @@ int main(){
         scanf("%d", &populacao);
 
         printf("Digite a área: ");
-        scanf("%d", &area);
+        scanf("%f", &area);
+
+        printf("Digite o PIB: ");
+        scanf("%f", &pib);
+
+        printf("Digite o número de pontos turísticos: ");
+        scanf("%d", &pontoturistico);
 
 		printf("Estado: %c\n", estado);
         printf("Código da Carta: %s\n", codigo);
         printf("Nome da Cidade: %s\n", cidade);
         printf("População: %d\n", populacao);
-        printf("Área: %.2f km²\n", area);
+        printf("Área: %f km²\n", area);
+        printf("PIB: %.2f bilhões de reais\n", pib);
+        printf("Número de pontos turísticos: %d\n", pontoturistico);
         
         
 
