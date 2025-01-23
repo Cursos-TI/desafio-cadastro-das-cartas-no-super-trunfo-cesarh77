51625e04f2654a981625008e4ab415ee383f580b
cesarh77 <cesarhenrique.tj@live.com>
2025-01-23 03:58:44 +0000

    Parte 4

--- DesafioPaises.c
+++ DesafioPaises.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
 
-    int main(){
+int main(){
 
-        char[15] estado;
 
-        printf("Digite o estado:");
-        scanf("%s" &estado);
+        int populacao;
+        char estado, cidade[20];
+        char codigo[3];
+        
+		
+		
+        printf("Digite o estado: ");
+        scanf("%c", &estado);
 
-        printf("Estado: %s, estado")
+        printf("Digite o código da carta: ");
+        scanf("%s", &codigo);
+		
+		printf("Digite o nome da cidade: ");
+        scanf("%s", &cidade);
+
+        printf("Digite a população: ");
+        scanf("%d", &populacao);
+
+		printf("Estado: %c\n", estado);
+        printf("Código da Carta: %s\n", codigo);
+        printf("Nome da Cidade: %s\n", cidade);
+        printf("População: %d\n", populacao);
+        
+        
+
+        return 0;
+    
 
 
 
